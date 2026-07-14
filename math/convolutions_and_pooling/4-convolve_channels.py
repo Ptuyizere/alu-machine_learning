9efce89f9426bc3c6b5dc730d6c510ea2f32b958
Ptuyizere <p.tuyizere1@alustudent.com>
2026-07-14 23:20:38 +0200

    corrected 4-convolve_grayscale.py

--- math/convolutions_and_pooling/4-convolve_channels.py
+++ math/convolutions_and_pooling/4-convolve_channels.py
@@ -1,77 +1,67 @@
 #!/usr/bin/env python3
 """
-Module for performing convolution on images with channels.
+    A function def
+    convolve(images, kernels, padding='same', stride=(1, 1)):
+    that performs a convolution on images using multiple kernels:
 """
+
 
 import numpy as np
 
 
 def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     """
-    Performs a convolution on images with channels.
+    Performs a convolution on images with multiple channels
+    using given padding and stride
 
-    Args:
-        images: numpy.ndarray of shape (m, h, w, c)
-        kernel: numpy.ndarray of shape (kh, kw, c)
-        padding: tuple (ph, pw), 'same', or 'valid'
-        stride: tuple (sh, sw)
+    parameters:
+        images [numpy.ndarray with shape (m, h, w, c)]:
+            contains multiple images
+            m: number of images
+            h: height in pixels of all images
+            w: width in pixels of all images
+            c: number of channels in the image
+        kernel [numpy.ndarray with shape (kh, kw, c)]:
+            contains the kernel for the convolution
+            kh: height of the kernel
+            kw: width of the kernel
+            c: number of channels in the image
+        padding [tuple of (ph, pw) or 'same' or 'valid']:
+            ph: padding for the height of the images
+            pw: padding for the width of the images
+            'same' performs same convolution
+            'valid' performs valid convolution
+        stride [tuple of (sh, sw)]:
+            sh: stride for the height of the image
+            sw: stride for the width of the image
 
-    Returns:
-        numpy.ndarray containing the convolved images.
+    if needed, images should be padded with 0s
+    function may only use two for loops maximum and no other loops are allowed
+
+    returns:
+        numpy.ndarray contained convolved images
     """
-
-    m, h, w, c = images.shape
+    m, height, width, c = images.shape
     kh, kw, kc = kernel.shape
     sh, sw = stride
-
-    # Calculate padding
     if padding == 'same':
-        output_h = int(np.ceil(h / sh))
-        output_w = int(np.ceil(w / sw))
-
-        pad_h = max((output_h - 1) * sh + kh - h, 0)
-        pad_w = max((output_w - 1) * sw + kw - w, 0)
-
-        ph = pad_h // 2
-        pw = pad_w // 2
-
+        ph = ((((height - 1) * sh) + kh - height) // 2) + 1
+        pw = ((((width - 1) * sw) + kw - width) // 2) + 1
     elif padding == 'valid':
         ph = 0
         pw = 0
-
-    elif isinstance(padding, tuple):
-        ph, pw = padding
-
     else:
-        raise ValueError("Invalid padding")
+        ph, pw = padding
+    images = np.pad(images, ((0, 0), (ph, ph), (pw, pw), (0, 0)),
+                    'constant', constant_values=0)
+    ch = ((height + (2 * ph) - kh) // sh) + 1
+    cw = ((width + (2 * pw) - kw) // sw) + 1
+    convoluted = np.zeros((m, ch, cw))
 
-    # Add zero padding
-    images_padded = np.pad(
-        images,
-        ((0, 0), (ph, ph), (pw, pw), (0, 0)),
-        mode='constant'
-    )
+    for i, h in enumerate(range(0, (height + (2 * ph) - kh + 1), sh)):
+        for j, w in enumerate(range(0, (width + (2 * pw) - kw + 1), sw)):
+            output = np.sum(images[:, h: h + kh, w: w + kw, :] * kernel,
+                            axis=(1, 2, 3))
+            convoluted[:, i, j] = output
 
-    # Output dimensions
-    output_h = ((h + 2 * ph - kh) // sh) + 1
-    output_w = ((w + 2 * pw - kw) // sw) + 1
-
-    output = np.zeros((m, output_h, output_w))
-
-    # Only two loops allowed
-    for i in range(output_h):
-        for j in range(output_w):
-
-            window = images_padded[
-                :,
-                i * sh:i * sh + kh,
-                j * sw:j * sw + kw,
-                :
-            ]
-
-            output[:, i, j] = np.sum(
-                window * kernel,
-                axis=(1, 2, 3)
-            )
-
-    return output
+    return convoluted
